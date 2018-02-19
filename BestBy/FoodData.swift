08764f91ac58286f9837a4e1bcf0f1fe2b089e3e
Erin Jensby <erin.jensby@yahoo.com>
2018-02-18 22:36:26 -0600

    writing food database

---
+++ BestBy/FoodData.swift
@@ -0,0 +1,36 @@
+//
+//  FoodData.swift
+//  BestBy
+//
+//  Created by Erin Jensby on 2/17/18.
+//  Copyright © 2018 Quatro. All rights reserved.
+//
+
+import Foundation
+
+struct FoodData {
+    
+    static let food_data:[String:(Int,String)] =
+        ["butter":(90, "It can be hard to tell is butter has gone bad. Butter that is spoiled may begin to look paler or smell stale or cheesy."),
+         "eggs": (35, "If there is a slight rotten odor when the egg is cracked it has spoiled.  You can also tell an egg has spoiled if the egg white has a pinkish or iridescent color."),
+         "sour cream": (30, "Sour Cream typically lasts 1-2 weeks past the printed date.  If the water in your sour cream begins to seperate you should finish it in the next few days.  If you see bright bacterial marks, dark mold, or notice a bitter flavor the sour cream has gone bad."),
+         "milk": (21, "Milk is typically still good for 5-7 days past the sell-by date. Milk that is discolored, lumpy, or has a sour odor has gone bad."),
+         "yogurt": (21, "Yogurt is typically still good for 7-10 days past the printed date. If you notice a curdling texture near the bottom of the container or an excess of liquid on the surface the yogurt is going bad.  If there is mold on the yogurt it has gone bad and must be thrown out."),
+         "apples": (45, "Apples stored in the fridge can last 1 to 2 months.  When the apple gets a grainy, soft interior and wrinkled skin or bruising the apple has gone bad."),
+         "avocados": (8, "Avocados stored in the fridge can last 7 to 10 days.  An avocado has gone bad once the inside is no longer green and will turn brown.  You can often tell it has gone bad before cutting into it if it has become so soft a light press leaves an indentation."),
+         "bananas": (6, "Bananas should be stored on the counter or on a banana stand.  Bananas that are going bad will have a large amount of the skin covered in brown spots and a softer texture.  Eventually they will turn completely brown and leak liquid and even grow mold."),
+         "blueberries": (8, "Blueberries stored in the fridge can last 5 to 10 days.  Blueberries that are going bad will beging to soften and have a mushy texture and some discoloration and bruising.  They will then grow mold and need to be thrown out."),
+         "grapes": (8, "Grapes stored in the fridge can last 5 to 10 days.  Grapes that are going bad will begin to have a softer texture and brown discoloration.  If grapes have mold or a vinegar smell they have gone bad and need to be thrown out."),
+         "lemons": (45, "Lemons can last 1 to 2 months in the fridge.  Lemons that are going bad have a soft texture and some discoloration.  If the lemon develops a soft spot or mold it needs to be thrown out."),
+         "limes": (45, "Limes can last 1 to 2 months in the fridge.  Limes that are going bad have a softer texture and a light brown colored discoloration on the skin.  The inside of the lime will also begin to dry out."),
+         "oranges": (45, "Oranges stored in the fridge can last 1 to 2 months.  Bad oranges will have a soft texture and some discoloration.  If the orange develops a soft spot or mold it need to be thrown out.  Bad oranges will also have a sour smell and taste."),
+         "peaches": (4, "Peaches in the fridge last 4-5 days.  Bad peaches will develop soft spots, overall softness, discoloration, and leaking."),
+         "pears": (8, "Pears in the fridge can last 5 to 12 days.  Bad pears will have multiple brown bruises or blemishes and a soft or brown center."),
+         "pineapples": (4, "Pineapples last 3 to 4 days and fridge storage is not necessary. Bad pineapples can have a soft or wet bottom, a sour, vinegar-like smell, darkened coloring, or a softer texture."),
+         "pomegrantes": (21, "A whole pomegranate can last 3 weeks in the fridge.  Once the seeds are removed they should be eaten in 5-7 days.  Update your pomegranates expiry reminder if you don't eat the seeds right away.  Pomegranates that have gone bad can be brown, brittle, or soft and the seeds may turn brown."),
+        "strawberries": (7, "Strawberries can last 5 to 7 days in the fridge.  Strawberries will be softer and discolored or bruised as they spoil.  If the strawberries grow mold they are bad and should be thrown out."),
+        "tomatoes": (14, "Tomatoes stored in the fridge can last 2 weeks.  Tomatoes will get softer and may leak fluid as they go bad. If the tomatoes grow mold they are bad and should be thrown out."),
+        "watermelon": (18, "Watermelon can last 2 to 3 weeks when stored in the fridge, if kept on the counter it will last 7-10 days.  A watermelon that has spoiled will have a soft, grainy texture, darken coloring, and have an excess of fluid in the center.")
+         ];
+    
+}
