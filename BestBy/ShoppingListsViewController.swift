2eeeeae1f2c3a9c5f15a116b13d231fb10cad280
Erin Jensby <erin.jensby@yahoo.com>
2018-03-03 15:22:30 -0600

    design changes

--- BestBy/ShoppingListsViewController.swift
+++ BestBy/ShoppingListsViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ShoppingListsViewController.swift
-//  
+//
 //
 //  Created by Erin Jensby on 2/27/18.
 //
@@ -8,7 +8,7 @@
 import UIKit
 
 class ShoppingListsViewController: UIViewController {
-
+    
     @IBOutlet weak var listTypeSegControl: UISegmentedControl!
     @IBOutlet weak var shopListsTableView: UITableView!
     
@@ -20,23 +20,22 @@ class ShoppingListsViewController: UIViewController {
         
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
 
 extension ShoppingListsViewController: UITableViewDelegate, UITableViewDataSource {
